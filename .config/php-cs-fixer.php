f9ac5ad829494df23c5c472022fd8efaa08b9995
Nicolas Hedger <nicolas@hedger.ch>
2022-04-06 20:23:35 +0200

    chore: Update code style rules
    Signed-off-by: Nicolas Hedger <nicolas@hedger.ch>

--- .config/php-cs-fixer.php
+++ .config/php-cs-fixer.php
@@ -14,7 +14,15 @@ $config->setRules([
     'declare_strict_types' => true,
     'php_unit_test_case_static_method_calls' => [
         'call_type' => 'this'
-    ]
+    ],
+    'php_unit_method_casing' => [
+        'case' => 'snake_case'
+    ],
+    'yoda_style' => [
+        'equal' => false,
+        'identical' => false,
+        'less_and_greater' => false,
+    ],
 ]);
 
 return $config
