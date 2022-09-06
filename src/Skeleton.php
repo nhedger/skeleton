b8c0064862c9b9bfc666853292146e2dd4876764
Nicolas Hedger <nicolas@hedger.ch>
2022-09-06 20:49:39 +0200

    feat: initial commit

---
+++ src/Skeleton.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hedger\Skeleton;
+
+class Skeleton
+{
+}
---
+++ tests/SkeletonTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hedger\Skeleton\Test;
+
+use Hedger\Skeleton\Skeleton;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Hedger\Skeleton\Skeleton
+ */
+class SkeletonTest extends TestCase
+{
+    public function test_it_works(): void
+    {
+        $this->assertInstanceOf(Skeleton::class, new Skeleton());
+    }
+}
